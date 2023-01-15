8d629ca53a96973e30ee1555f03042dfd69d8655
Dmitry Yavtushenko <rosengan22@gmail.com>
2023-01-15 23:20:26 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 
+
+console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const murkup = galleryItems.map(el => {
+    const cardMarkup = `
+  <a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`;
+        return cardMarkup;
+    }).join('');
+
+galleryRef.innerHTML = murkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+// *******************
 console.log(galleryItems);
