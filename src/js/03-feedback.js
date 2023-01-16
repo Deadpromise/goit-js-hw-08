9991ad154583761db02d6ba8de0fd9ceea5135c1
Dmitry Yavtushenko <rosengan22@gmail.com>
2023-01-16 02:19:30 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('[name="email"]');
+const textareaRef = document.querySelector('[name="message"]');
+
+const LOCAL_FORM_DATA = "feedback-form-state";
+const savedValues = localStorage.getItem(LOCAL_FORM_DATA);
+const formData = JSON.parse(savedValues) || {};
+
+setSavedData();
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+     formData[e.target.name] = e.target.value;
+    localStorage.setItem(LOCAL_FORM_DATA, JSON.stringify(formData));
+};
+
+function setSavedData() {
+    if (savedValues) {
+        const parsedValues = JSON.parse(savedValues);
+        emailRef.value = parsedValues.email;
+        textareaRef.value = parsedValues.message;
+    }
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formData);
+    evt.currentTarget.reset();
+    localStorage.removeItem(LOCAL_FORM_DATA);   
+};
+
+
+
 
